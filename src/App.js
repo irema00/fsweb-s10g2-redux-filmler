35a3b83718c6cafd501fe0bd36a8d165b7497277
İrem Akın Tunga <iremakin00@gmail.com>
2023-11-22 22:50:56 +0300

    adds toggleFavorites

--- src/App.js
+++ src/App.js
@@ -2,17 +2,20 @@ import React from "react";
 
 import { Route, Switch, Redirect } from "react-router-dom";
 
-import MovieList from './components/MovieList';
-import Movie from './components/Movie';
+import MovieList from "./components/MovieList";
+import Movie from "./components/Movie";
 
-import AppHeader from './components/AppHeader';
+import AppHeader from "./components/AppHeader";
 
-import AddMovieForm from './components/AddMovieForm';
-import FavoriteMovieList from './components/FavoriteMovieList';
+import AddMovieForm from "./components/AddMovieForm";
+import FavoriteMovieList from "./components/FavoriteMovieList";
+import { useSelector } from "react-redux";
+const App = (props) => {
+  const displayFavorites = useSelector(
+    (state) => state.favorites.favoritesVisible
+  );
 
-const App = props => {
-  const displayFavorites = true;
-
+  console.log("FAV GOSTER", displayFavorites);
   return (
     <div>
       <nav className="bg-zinc-800 px-6 py-3">
