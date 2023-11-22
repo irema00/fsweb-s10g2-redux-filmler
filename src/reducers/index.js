273de8d6a53fc431979eb6ea10e591acad405d01
İrem Akın Tunga <iremakin00@gmail.com>
2023-11-22 16:04:34 +0300

    updates store in index.js

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,9 @@
-import { combineReducers } from 'redux';
+import { combineReducers, legacy_createStore as createStore } from "redux";
 
-import movieReducer from './movieReducer';
+import movieReducer from "./movieReducer";
 
+const reducers = combineReducers({
+  movie: movieReducer,
+});
 export default movieReducer;
+export const store = createStore(reducers);
