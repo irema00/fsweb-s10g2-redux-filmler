6b0271f3ce6f2eaaf445c54cfe45c04fb377821c
İrem Akın Tunga <iremakin00@gmail.com>
2023-11-22 22:22:47 +0300

    creates favorites reducer

--- src/reducers/favoritesReducer.js
+++ src/reducers/favoritesReducer.js
@@ -1,4 +1,20 @@
-const favoritesInitialState = {
-  favorites: [],
-  displayFavorites: false,
+import {
+  TOGGLE_FAVORITES,
+  ADD_FAVORITE,
+  REMOVE_FAVORITE,
+} from "../actions/movieActions.js";
+import movies from "../data.js";
+
+const initialState = {
+  favorites: [movies[0]],
+  favoritesVisible: true,
 };
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+export default reducer;
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,11 @@
 import { combineReducers, legacy_createStore as createStore } from "redux";
 
 import movieReducer from "./movieReducer";
+import favoritesReducer from "./favoritesReducer";
 
 const reducers = combineReducers({
   movie: movieReducer,
+  favorites: favoritesReducer,
 });
 export const store = createStore(reducers);
 export default reducers;
